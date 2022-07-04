75c49d817de1f19ba643f8ce5de5e0bf7f12de88
HinnerAsuka <1838557277@qq.com>
2022-07-04 13:01:21 +0800

    购物车redis的增删改查及优化

--- meiduo_mall/apps/carts/views.py
+++ meiduo_mall/apps/carts/views.py
@@ -34,10 +34,16 @@ class CartsView(View):
             redis_cli = get_redis_connection('carts')
             # 用哈希格式保存
             # hset(key, field, value)
-            redis_cli.hset(f'carts_{user.id}', sku_id, count)
+            # redis_cli.hset(f'carts_{user.id}', sku_id, count)
 
-            redis_cli.sadd(f'selected_{user.id}', sku_id)
+            # 通过管道获取数据
+            pipeline = redis_cli.pipeline()
 
+            # hincrby会进行累加操作
+            pipeline.hincrby(f'carts_{user.id}', sku_id, count)
+            # 用集合保存选中商品
+            pipeline.sadd(f'selected_{user.id}', sku_id)
+            pipeline.execute()  # 执行管道
             return JsonResponse({'code': 0})
         else:
             # 未登录进行cookie操作
@@ -68,4 +74,129 @@ class CartsView(View):
             response.set_cookie('carts', base64_encode.decode(), max_age=3600 * 24)
             return response
 
+    # 购物车的展示
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            # 已登录操作redis
+            redis_cli = get_redis_connection('carts')
+            pipeline = redis_cli.pipeline()
+            sku_id_count = pipeline.hgetall(f'carts_{user.id}')
+            selected_ids = pipeline.smembers(f'selected_{user.id}')
+            pipeline.execute()
+            # 将redis数据转换为和cookie一样的数据
+            carts = {}
+            for sku_id, count in sku_id_count.items():
+                carts[int(sku_id)] = {
+                    'count': int(count),
+                    'selected': sku_id in selected_ids
+                }
+        else:
+            # 未登录操作cookie
+            # 先读取cookie
+            cookie_carts = request.COOKIES.get('carts')
+            if cookie_carts:
+                # 解密
+                carts = pickle.loads(base64.b64decode(cookie_carts))
+            else:
+                carts = {}
 
+        sku_ids = carts.keys()
+        skus = SKU.objects.filter(id__in=sku_ids)
+        sku_list = []
+        for sku in skus:
+            sku_list.append(
+                {
+                    'id': sku.id,
+                    'name': sku.name,
+                    'price': sku.price,
+                    'default_image_url': sku.default_image.url,
+                    'selected': carts[sku.id]['selected'],  # 选中状态
+                    'count': int(carts[sku.id]['count']),    # 数量
+                    'amount': sku.price * carts[sku.id]['count']    # 总价格
+                }
+            )
+        return JsonResponse({'code': 0, 'cart_skus': sku_list})
+
+    # 修改购物车数据
+    def put(self, request):
+        user = request.user
+        data = json.loads(request.body)
+
+        sku_id = data.get('sku_id')
+        count = data.get('count')
+        selected = data.get('selected')
+
+        if not all([sku_id, count]):
+            return JsonResponse({'code': 400})
+
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return JsonResponse({'code': 400})
+
+        try:
+            count = int(count)
+        except:
+            count = 1
+
+        if user.is_authenticated:
+            redis_cli = get_redis_connection('carts')
+            pipeline = redis_cli.pipeline()
+            pipeline.hset(f'carts_{user.id}', sku_id, user.id)
+
+            if selected:    # selected状态为True
+                pipeline.sadd(f'selected_{user.id}', sku_id)
+            else:
+                pipeline.srem(f'selected_{user.id}', sku_id)
+            pipeline.execute()
+
+            return JsonResponse({'code': 0, 'cart_sku': {'count': count, 'selected': selected}})
+        else:
+            cookie_carts = request.COOKIES.get('carts')
+            if cookie_carts:
+                # 解密
+                carts = pickle.loads(base64.b64decode(cookie_carts))
+            else:
+                carts = {}
+            if sku_id in carts:
+                carts[sku_id] = {
+                    'count': count,
+                    'selected': selected
+                }
+
+            new_carts = base64.b64encode(pickle.dumps(carts))
+            response = JsonResponse({'code': 0, 'cart_sku': {'count': count, 'selected': selected}})
+            response.set_cookie('carts', new_carts.decode(), max_age=3600 * 24)
+            return response
+
+    # 删除购物车数据
+    def delete(self, request):
+        data = json.loads(request.body)
+        sku_id = data.get('sku_id')
+
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return JsonResponse({'code': 400})
+
+        user = request.user
+        if user.is_authenticated:
+            redis_cli = get_redis_connection('carts')
+            redis_cli.hdel(f'carts_{user.id}', sku_id)
+            redis_cli.srem(f'selected_{user.id}', sku_id)
+
+            return JsonResponse({'code': 0})
+        else:
+            cookie_carts = request.COOKIES.get('carts')
+            if cookie_carts:
+                # 解密
+                carts = pickle.loads(base64.b64decode(cookie_carts))
+            else:
+                carts = {}
+
+            del carts[sku_id]
+            new_carts = base64.b64encode(pickle.dumps(carts))
+            response = JsonResponse({'code': 0})
+            response.set_cookie('carts', new_carts.decode(), max_age=3600 * 24)
+            return response

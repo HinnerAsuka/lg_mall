58869fb44a3512c89ae58fffbf46ce83ff9c4a30
HinnerAsuka <1838557277@qq.com>
2022-07-07 18:31:35 +0800

    生成支付宝的交易信息并更新订单状态

--- meiduo_mall/apps/pay/urls.py
+++ meiduo_mall/apps/pay/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.pay.views import PayUrlView
+from apps.pay.views import PayUrlView, PaymentStatusView
 
 
 urlpatterns = [
+    path('payment/status/', PaymentStatusView.as_view()),
     path('payment/<order_id>/', PayUrlView.as_view()),
 ]
--- meiduo_mall/apps/pay/views.py
+++ meiduo_mall/apps/pay/views.py
@@ -9,7 +9,9 @@ from utils.views import LoginRequiredJSONMixin
 from meiduo_mall import settings
 from alipay import AliPay, AliPayConfig
 
+# 沙箱测试账号    wkqslg7343@sandbox.com  密码 111111 支付密码 111111
 
+# 生成支付宝跳转链接
 class PayUrlView(LoginRequiredJSONMixin, View):
 
     def get(self, request, order_id):
@@ -54,3 +56,40 @@ class PayUrlView(LoginRequiredJSONMixin, View):
         pay_url = 'https://openapi.alipaydev.com/gateway.do?' + order_string
         # 返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'alipay_url': pay_url})
+
+from apps.pay.models import Payment
+
+
+class PaymentStatusView(View):
+
+    def put(self, request):
+        data = request.GET
+
+        data = data.dict()
+        signature = data.pop('sign')
+
+        app_private_key_string = open(settings.APP_PRIVATE_KEY_PATH).read()
+        alipay_public_key_string = open(settings.ALIPAY_PUBLIC_KEY_PATH).read()
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_string=app_private_key_string,
+            # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            alipay_public_key_string=alipay_public_key_string,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG,  # 默认False
+            config=AliPayConfig(timeout=15)  # 可选, 请求超时时间
+        )
+        success = alipay.verify(data, signature)
+        if success:
+            trade_no = data.get('trade_no')  # 支付宝交易号
+            order_id = data.get('out_trade_no')  # 商家订单号
+            Payment.objects.create(
+                trade_id=trade_no,
+                order_id=order_id
+            )
+            # 更新订单状态
+            OrderInfo.objects.filter(order_id=order_id).update(status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
+            return JsonResponse({'code': 0, 'trade_id': trade_no})
+        else:
+            return JsonResponse({'code': 400})

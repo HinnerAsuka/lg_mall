b1766d4b0f38ce6b9044dc59b43d66869f3b64e8
HinnerAsuka <1838557277@qq.com>
2022-07-15 19:21:17 +0800

    首页其他功能的实现

--- meiduo_mall/apps/admin_manage/urls.py
+++ meiduo_mall/apps/admin_manage/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from apps.admin_manage.user import legou_jwt_token
-from apps.admin_manage.views.dayactive import DailyActiveAPIView
+from apps.admin_manage.views.dayactive import DailyActiveAPIView, DailyOrderAPIView, TotalUserAPIView,\
+    DailyIncrementAPIView, MonthIncrementAPIView
 
 urlpatterns = [
     path('authorizations/', legou_jwt_token),
     path('statistical/day_active/', DailyActiveAPIView.as_view()),
+    path('statistical/day_orders/', DailyOrderAPIView.as_view()),
+    path('statistical/total_count/', TotalUserAPIView.as_view()),
+    path('statistical/day_increment/', DailyIncrementAPIView.as_view()),
+    path('statistical/month_increment/', MonthIncrementAPIView.as_view()),
 ]
--- meiduo_mall/apps/admin_manage/views/dayactive.py
+++ meiduo_mall/apps/admin_manage/views/dayactive.py
@@ -1,16 +1,49 @@
 from apps.users.models import User
-
-
-# 日活用户统计
-
 from datetime import date
 from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
+# 日活用户统计
 class DailyActiveAPIView(APIView):
 
     def get(self, request):
         today = date.today()
         count = User.objects.filter(last_login__gte=today).count()
+        return Response({'count': count})
+
+
+# 日下单用户统计
+class DailyOrderAPIView(APIView):
+
+    def get(self, request):
+        count = User.objects.filter(orderinfo__create_time__gte=date.today()).count()
+        return Response({'count': count})
+
+
+# 用户总量
+class TotalUserAPIView(APIView):
+
+    def get(self, request):
+        count = User.objects.filter(is_active=1).count()
+        return Response({'count': count})
+
+
+# 日增用户
+class DailyIncrementAPIView(APIView):
+
+    def get(self, request):
+        count = User.objects.filter(date_joined__gte=date.today()).count()
+        return Response({'count': count})
+
+
+from datetime import datetime
+
+
+# 月增用户
+class MonthIncrementAPIView(APIView):
+
+    def get(self, request):
+        count = User.objects.filter(date_joined__month=date.today().month).count()
         return Response({'count': count})
